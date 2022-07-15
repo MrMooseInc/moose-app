5b87b23bd8e0d93efb2c63c14b8249a649611b6a
greg <gregorybwest@gmail.com>
2022-07-14 19:17:57 -0700

    crud actions for tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  def index
+    tasks = Task.all
+    render json: tasks
+  end
+
+  def create
+    tasks = Task.new(
+      name: params[:name],
+      doses_required: params[:doses_required],
+      doses_given:params[:doses_given]
+    )
+    if task.save
+      render json: task, status: :created
+    else
+      render json: {task.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  def show
+    task = Task.find(params[:id])
+    render json: task
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.name = params[:name] || task.name
+    task.doses_required = params[:doses_required] || task.doses_required
+    task.doses_given = params[:doses_given] || task.doses_given
+    if task.save
+      render json: task, status: :created
+    else
+      render json: {task.errors.full_messages}, status: :unprocessible_entity
+    end
+    
+    def destroy
+     task = Task.find(params[:id]) 
+     task.destroy
+     render json: {message: "Task successfully destroyed!"}
+    end
+
+  end
+
+end
